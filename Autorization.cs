4ff22e161bc77c2066a3b4b73b3096a07e3b7432
DenisKonovalov <ds.konovalov@mail.ru>
2024-06-25 01:53:32 +0300

    Update Autorization.cs
    Update authorization

--- Autorization.cs
+++ Autorization.cs
@@ -7,6 +7,10 @@ public class Autorization
 {
 	public virtual void NewPatient()
 	{
+		int SNILS  = 167000000;
+		string FIO = "Коновалов Д.С.";
+		string Password = "qwerty123";
+		Patient p = new Patient(SNILS, FIO, Password);
 		throw new System.NotImplementedException();
 	}
 
