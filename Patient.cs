6d88346937d0da6344878ee593cd0aae263e8124
DenisKonovalov <ds.konovalov@mail.ru>
2024-06-25 01:05:59 +0300

    Create Patient.cs
    init

---
+++ Patient.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Patient
+{
+	public virtual int SNILS
+	{
+		get;
+		set;
+	}
+
+	public virtual string Password
+	{
+		get;
+		set;
+	}
+
+	public virtual string FIO
+	{
+		get;
+		set;
+	}
+
+	public virtual string GetNewPassword()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual List<Appointment> CurrentApps()
+	{
+		throw new System.NotImplementedException();
+	}
+
+}
+
